e9b3ee54fa1264cad7abd0640f60cab82b9a0e99
khabane <seretsi@users.noreply.github.com>
2017-09-15 13:18:28 -0400

    cosmetic commit to remove build files 1

--- export/windows/neko/haxe/ApplicationMain.hx
+++ export/windows/neko/haxe/ApplicationMain.hx
@@ -57,8 +57,8 @@ class ApplicationMain {
 				
 				#if mobile
 				
-				forceWidth = 640;
-				forceHeight = 480;
+				forceWidth = 800;
+				forceHeight = 600;
 				
 				container = new flash.display.Sprite ();
 				barA = new flash.display.Sprite ();
@@ -127,7 +127,7 @@ class ApplicationMain {
 				}
 				
 			},
-			640, 480, 
+			800, 600, 
 			60, 
 			0,
 			(true ? flash.Lib.HARDWARE : 0) |
@@ -248,13 +248,13 @@ class ScaledStage extends flash.display.Stage {
 	
 	private override function get_stageHeight ():Int {
 		
-		return 480;
+		return 600;
 	
 	}
 	
 	private override function get_stageWidth ():Int {
 		
-		return 640;
+		return 800;
 	
 	}
 	
@@ -325,11 +325,11 @@ class ApplicationMain {
 		wx.App.boot (function () {
 			
 			
-			frame = wx.Frame.create (null, null, "gamedev1p1g9", null, { width: 640, height: 480 });
+			frame = wx.Frame.create (null, null, "gamedev1p1g9", null, { width: 800, height: 600 });
 			
 			
 			#if openfl
-			var stage = wx.NMEStage.create (frame, null, null, { width: 640, height: 480 });
+			var stage = wx.NMEStage.create (frame, null, null, { width: 800, height: 600 });
 			#end
 			
 			var hasMain = false;
@@ -533,13 +533,13 @@ class ApplicationMain {
 					display: 0,
 					fullscreen: false,
 					hardware: true,
-					height: 480,
+					height: 600,
 					parameters: "{}",
 					resizable: true,
 					stencilBuffer: false,
 					title: "gamedev1p1g9",
 					vsync: true,
-					width: 640,
+					width: 800,
 					x: null,
 					y: null
 				},
@@ -549,7 +549,7 @@ class ApplicationMain {
 		
 		#if (js && html5)
 		#if (munit || utest)
-		openfl.Lib.embed (null, 640, 480, "null");
+		openfl.Lib.embed (null, 800, 600, "null");
 		#end
 		#else
 		create ();
