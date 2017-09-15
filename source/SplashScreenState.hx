4981cba06e598ea70c25e26f8a02f437e57b0699
khabane <seretsi@users.noreply.github.com>
2017-09-15 14:54:31 -0400

    Added button
    "Press Any Key" button added to splash screen.

--- source/SplashScreenState.hx
+++ source/SplashScreenState.hx
@@ -3,14 +3,25 @@ package;
 import flixel.FlxG;
 import flixel.FlxState;
 import flixel.FlxSprite;
+import flixel.text.FlxText;
+import flixel.ui.FlxButton;
 
 class SplashScreenState extends FlxState{
-	var _title = new FlxSprite( 275, 150);
-
+	// params set location of title draw. Will need to change according to resolution of actual title Sprite
+	var _title = new FlxSprite( 275, 150); 
+	//button needed to be initialized in the create() function to avoid issues
+	var _anyKeyButton:FlxButton;
+	
 	override public function create():Void{
 		super.create();
 		_title.loadGraphic("assets/images/SplashScreenGameTitle_Placeholder.png", true, 248, 150);
+		_anyKeyButton  = new FlxButton(10, 10, "press any key to start", clickPlay);
+		_anyKeyButton.x = 360;
+		_anyKeyButton.y = 325;
+
+		//add objects to render list
 		add(_title);
+		add(_anyKeyButton);
 	}
 
 	override public function update(elapsed:Float):Void {
@@ -18,6 +29,6 @@ class SplashScreenState extends FlxState{
 	}
 
 	function clickPlay():Void{
-		FlxG.switchState(new MenuState());
+		FlxG.switchState(new MenuState()); // we may not have a menu and just jump straight into the game
 	}
 }
