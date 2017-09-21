7a9beea0cd213bc869ebbfad04d4bdc0f7cc507f
upscalerat <heather.siobhans@gmail.com>
2017-09-21 19:39:13 -0400

    Placehold player, splashscreen to menu to play

--- Player.hx
+++ Player.hx
@@ -1,61 +1,91 @@
 package;
 
 /**
- * ...
  * @author Becca Sheridan
  */
+ 
+ import flixel.FlxG;
  import flixel.FlxSprite;
  import flixel.system.FlxAssets.FlxGraphicAsset;
+ import flixel.math.FlxPoint;
+ import flixel.FlxObject;
+ 
+ import flixel.util.FlxColor;
 
- class Player extends FlxSprite
- {
+ class Player extends FlxSprite {
 	 
-	 var speed:Float = 0;
-	 var _drift:Float = 0;
+	 var speed:Float = 200;
 	 var _up:Bool = false;
 	 var _down:Bool = false;
 	 var _right:Bool = false;
 	 var _left:Bool = false;
-	 var CONSTANT _width:Int = 10;
-	 var CONSTANT _height:Int = 10;
+	 var WIDTH:Int = 300;
+	 var HEIGHT:Int = 700;
+	 var coupons:Int = 0;
 	 
-     public function new(?X:Float=0, ?Y:Float=0, ?SimpleGraphic:FlxGraphicAsset)
-     {
-         super(X, Y, SimpleGraphic);
-		 //loadGraphic("assets/images/SPRITENAME.filetype", true, _width, _height);
-		 //And like. some spriting stuff. i'll get to 
+     public function new(?X:Float=0, ?Y:Float=0)     {
+         super(X, Y);
+		 //loadGraphic("assets/images/IMGNAME.png", true, WIDTH, HEIGHT);
+		 makeGraphic(16, 16, FlxColor.BLUE);
+		 //And like. some spriting stuff. i'll get to
+		 drag.x = drag.y = 1600;
      }
 	 
-	 override public function update(elapsed:Float):Void{
-		 super.update(elapsed);
-		 poll();
+	 override public function update(elapsed:Float):Void	{
 		 movement();
-		 //tbh i dont know what this does but like w/e
+		 super.update(elapsed);
 	 }
+	
+	function movement():Void	{
+		
+	 _up = FlxG.keys.anyPressed([UP, W]);
+	 _down = FlxG.keys.anyPressed([DOWN, S]);
+	 _right = FlxG.keys.anyPressed([RIGHT, D]);
+	 _left = FlxG.keys.anyPressed([LEFT, A]);
 	 
-	 function poll():Void{
-		_up = FlxG.keys.anyPressed([UP, W]);
-		_down = FlxG.keys.anyPressed([DOWN, S]);
-		_left = FlxG.keys.anyPressed([LEFT, A]);
-		_right = FlxG.keys.anyPressed([RIGHT, D]);		
-	}
-	
-	function movement():Void{
+	 if (_up && _down){
+		 _up = _down = false;
+	 }
+	 if (_right && _left){
+		 _right = _left = false;
+	 }
+		
 		if (_up || _down || _left || _right){
-			if (_left){
-				_rot = 180;
-				facing  = FlxObject.LEFT;
+			velocity.x = speed;
+			velocity.y = speed;
+			var mA:Float = 0;
+			if (_up){
+				mA =-90;
+				if (_left){
+					mA -= 45;
+				} else if (_right){
+					mA += 45;
+				}
+			} else if (_down){
+				mA = 90;
+				if (_left){
+					mA += 45;
+				} else if (_right){
+					mA -= 45;
+				}
+			} else if (_left){
+				mA = 180;
+			} else if (_right){
+				mA = 0;
 			}
-			else if (_right){
-				_rot = 0;
-				facing = FlxObject.RIGHT;
-			}
-			velocity.set (speed, 0);
-			velocity.rotate(new FlxPoint(0,0), _rot);
-			//animation.play("walk");
+			velocity.set(speed, 0);
+			velocity.rotate(FlxPoint.weak(0, 0), mA);
 		}
-		else animation.stop();
+		
+	}
+	/*
+	function gainCoupon():Void	{
+		coupons = coupons + 1;
 	}
 	
+	function canLeave(coupNum:Int):Bool	{
+		return coupons == coupNum;
+	}
+	*/
 	//Definitely lifted this from Marc's example code. Definitely will tailor to our stuff with sprites
  }
