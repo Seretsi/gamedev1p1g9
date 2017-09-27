40fb78e77e32ff6ba190f34c9c8702ff2db361cb
Millmoss <omelliff@yahoo.com>
2017-09-26 21:42:24 -0400

    Nothing from my merge updated at all agggggggghhhhhhh

--- source/LevelOneState.hx
+++ source/LevelOneState.hx
@@ -55,6 +55,15 @@ class LevelOneState extends FlxState {
 			ui.setInteractText(2);
 			ui.setMonologueText(2);
 		}
+		if (LEVEL END)
+		{
+			ui.getEndResults();
+			add(ui.getEndHeaderScoreItem());
+			add(ui.getEndMoneyScoreItem());
+			add(ui.getEndCoupScoreItem());
+			add(ui.getEndTimeScoreItem());
+			add(ui.getEndTotalScoreItem());
+		}
 		*/
 	}
 }
