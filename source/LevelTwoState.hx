9c7f7b70cdd2ebd99cb4972bbb10316de932725f
upscalerat <heather.siobhans@gmail.com>
2017-09-27 03:13:56 -0400

    Coupons in all levels. Lvl 2, 3 exist

--- source/LevelTwoState.hx
+++ source/LevelTwoState.hx
@@ -2,34 +2,85 @@ package;
 
 import flixel.FlxState;
 import flixel.util.FlxColor;
+import flixel.FlxG;
+import flixel.tile.FlxTilemap;
+import flixel.addons.editors.ogmo.FlxOgmoLoader;
+import flixel.FlxSprite;
+import flixel.FlxObject;
+import flixel.util.FlxCollision;
 
 class LevelTwoState extends FlxState {
 	var ui:UIFunctions;
-	
+	var player:Player = new Player(655,225);
+	var _map:FlxOgmoLoader;
+	var _mWalls:FlxTilemap;
+	var cam:Camera = new Camera();
+	var _bg = new FlxSprite( 1, 1);
+	private var numCoups:Int = 3;
+	private var coupsHad:Int = 0;
+	private var canLeave:Bool = false; 
+	var _coupon1:Coupon = new Coupon(300, 105);
+	var _coupon2:Coupon = new Coupon(350, 500);
+	var _coupon3:Coupon = new Coupon(50, 470);
+	var _coupon4:Coupon = new Coupon(5, 25);
+
 	override public function create():Void {
+		cam.setTarget(player);
+		FlxG.camera = cam;
 		super.create();
-		bgColor = FlxColor.BLACK;
-		var levelTime:Float = 10;
+		bgColor = FlxColor.WHITE;
 		ui = new UIFunctions(10, 1);
+		
+		_bg.loadGraphic("assets/art-refined/lv2.png", true, 3200, 2400);
+		_bg.setGraphicSize(800);
+		_bg.screenCenter();
+		add(_bg);
+
+		_map = new FlxOgmoLoader("assets/images/levelOneCollisions.oel");
+		_mWalls = _map.loadTilemap("assets/art-refined/lv1.png", 100, 100, "walls");
+		//_mWalls.loadMapFromGraphic("assets/art-refined/lv1.png", false, 100, null, /*TileGraphic:FlxTilemapGraphicAsset*/null,
+										 //100, 100, flixel.tile.FlxBaseTilemap.FlxTilemapAutoTiling.AUTO, 0, 1, 1);
+		_mWalls.setTileProperties(1, FlxObject.NONE);
+		/*_mWalls.follow();
+		*/
+		_mWalls.setTileProperties(2, FlxObject.ANY);
+		add(player);
+		add(_mWalls);
 		add(ui.getMonologueItem());
 		add(ui.getInteractItem());
 		add(ui.getCouponsItem());
 		add(ui.getScoresItem());
 		add(ui.getTimerItem());
+		
+		add(_coupon1);
+		add(_coupon2);
+		add(_coupon3);
+		add(_coupon4);
 	}
 
 	override public function update(elapsed:Float):Void {
 		super.update(elapsed);
 		ui.updateUI(elapsed);
+		FlxG.overlap(player, _coupon1, onCoupCollision);
+		FlxG.overlap(player, _coupon2, onCoupCollision);
+		FlxG.overlap(player, _coupon3, onCoupCollision);
+		FlxG.overlap(player, _coupon4, onCoupCollision);
 		/*if (CRASH) {
 			ui.setInteractText(1);
 			ui.setMonologueText(1, 1);
 		}
-		if (COUPON)
-		{
-			ui.setInteractText(2);
-			ui.setMonologueText(2, 1);
-		}
 		*/
+	}
+
+	private function onCoupCollision(player:Player, coupon:Coupon){
+		if (coupon.exists && player.exists){
+			//FlxG.sound.play(collectSoundName);
+			ui.setCoupons(coupon);
+			coupon.destroy();
+			coupsHad++;
+			ui.setInteractText(2);
+			ui.setMonologueText(2);
+			if (coupsHad >= numCoups){canLeave = true;}
+		}
 	}
 }
