3dd80a6f83eefe7196f22cd5788e4b4b15384e4f
Millmoss <omelliff@yahoo.com>
2017-09-26 23:03:36 -0400

    Completed UIFunctions added to Master

--- source/LevelOneState.hx
+++ source/LevelOneState.hx
@@ -14,6 +14,7 @@ class LevelOneState extends FlxState {
 	var _mWalls:FlxTilemap;
 	var cam:Camera = new Camera();
 	var _bg = new FlxSprite( 1, 1); 
+	var temp:Bool;
 
 	override public function create():Void {
 		cam.setTarget(player);
@@ -22,11 +23,18 @@ class LevelOneState extends FlxState {
 		bgColor = FlxColor.BLACK;
 		var levelTime:Float = 10;
 		ui = new UIFunctions(10, 1);
+<<<<<<< HEAD
+<<<<<<< HEAD
+		temp = true;
+=======
+>>>>>>> fdf5e25ce659e316cc4d27f12bec6e6eedd2c064
+=======
+>>>>>>> fdf5e25ce659e316cc4d27f12bec6e6eedd2c064
 		
 		_bg.loadGraphic("assets/art-refined/lv1.png", true, 3200, 2400);
 		_bg.setGraphicSize(800);
 		_bg.screenCenter();
-		add(_bg);
+		//add(_bg);
 
 		//_map = new FlxOgmoLoader(AssetPaths.levelOneCollisions_oel);
 		//_mWalls = _map.loadTilemap(AssetPaths.tiles__png, 16, 16, "walls");
@@ -55,13 +63,14 @@ class LevelOneState extends FlxState {
 			ui.setInteractText(2);
 			ui.setMonologueText(2);
 		}
-		if(LEVEL END) {
+		if(LEVEL END) { //ONLY CALL ALL OF THESE ONCE!
 			ui.getEndResults();
 			add(ui.getEndHeaderScoreItem());
 			add(ui.getEndMoneyScoreItem());
 			add(ui.getEndCoupScoreItem());
 			add(ui.getEndTimeScoreItem());
 			add(ui.getEndTotalScoreItem());
+			LEVEL END = false;
 		}
 		*/
 	}
--- source/UIFunctions.hx
+++ source/UIFunctions.hx
@@ -66,6 +66,7 @@ class UIFunctions {
 		monologueTimer = new FlxTimer();
 		interactTimer = new FlxTimer();
 		endScoreTimer = new FlxTimer();
+		endScoreTimer.active = false;
 	}
 	
 	public function updateUI(elapsed):Void {
@@ -81,6 +82,19 @@ class UIFunctions {
 		timerItem.text = timerText;
 		runMono();
 		runInteract();
+		if (endScoreTimer.active == true)
+		{
+			if (endScoreTimer.elapsedTime > 2 && endMoneyScoreItem.alpha == 0)
+				endMoneyScoreItem.alpha = 1;
+			if (endScoreTimer.elapsedTime > 4 && endCoupScoreItem.alpha == 0)
+				endCoupScoreItem.alpha = 1;
+			if (endScoreTimer.elapsedTime > 6 && endTimeScoreItem.alpha == 0)
+				endTimeScoreItem.alpha = 1;
+			if (endScoreTimer.elapsedTime > 8 && endTotalScoreItem.alpha == 0)
+				endTotalScoreItem.alpha = 1;
+		}
+		if (endScoreTimer.active == true && endScoreTimer.elapsedTime > 10)
+			endScoreTimer.active = false;
 	}
 	
 	public function setCoupons(c:Coupon):Void {
@@ -159,7 +173,9 @@ class UIFunctions {
 	public function getEndResults():Void {
 	//all level assets alpha values should be set to 0 or assets destroyed at this point
 		monologueItem.alpha = 0;
+		monologueTimer.active = false;
 		interactItem.alpha = 0;
+		interactTimer.active = false;
 		couponsItem.alpha = 0;
 		scoresItem.alpha = 0;
 		timerItem.alpha = 0;
@@ -167,12 +183,16 @@ class UIFunctions {
 		endHeaderScoreItem.setFormat("assets/fonts/seguibl.ttf", 16, textColor, "center");
 		endMoneyScoreItem = new FlxText(0, 200, 800, "");
 		endMoneyScoreItem.setFormat("assets/fonts/seguibl.ttf", 16, textColor, "center");
+		endMoneyScoreItem.alpha = 0;
 		endCoupScoreItem = new FlxText(0, 250, 800, "");
 		endCoupScoreItem.setFormat("assets/fonts/seguibl.ttf", 16, textColor, "center");
+		endCoupScoreItem.alpha = 0;
 		endTimeScoreItem = new FlxText(0, 300, 800, "");
 		endTimeScoreItem.setFormat("assets/fonts/seguibl.ttf", 16, textColor, "center");
+		endTimeScoreItem.alpha = 0;
 		endTotalScoreItem = new FlxText(0, 350, 800, "");
 		endTotalScoreItem.setFormat("assets/fonts/seguibl.ttf", 16, textColor, "center");
+		endTotalScoreItem.alpha = 0;
 		endHeaderScoreItem.text = "LEVEL " + lvl + " SCORE";
 		var ms:Int = -200;
 		if (lvl == 2)
@@ -186,10 +206,7 @@ class UIFunctions {
 		endCoupScoreItem.text = "Coupons Collected: " + cs + " points";
 		var total:Int = ms + ts + cs;
 		endTotalScoreItem.text = "Total Score: " + total + " points";
-	}
-	
-	public function updateEndResults(elapsed) {
-		
+		endScoreTimer.start(10.5);
 	}
 	
 	public function getMonologueItem():FlxText {
