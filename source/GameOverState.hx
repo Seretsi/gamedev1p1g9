cee3cea7371ec558777d6f56e57387f041bfa56a
khabane <seretsi@users.noreply.github.com>
2017-09-27 14:03:30 -0400

    text shift in game over

--- source/GameOverState.hx
+++ source/GameOverState.hx
@@ -26,7 +26,7 @@ class GameOverState extends FlxState
 	{
 		super.create();
 		_cam.gameOver();
-		_gameOverTxt = new FlxText(300, 300, 300, "You starved to death and died. Game Over!");
+		_gameOverTxt = new FlxText(290, 300, 300, "You starved to death and died. Game Over!");
 		_exitKeyButton  = new FlxButton(10, 10, "EXIT", returnToSplashScreen);
 		_exitKeyButton.x = 300;
 		_exitKeyButton.y = 325;
