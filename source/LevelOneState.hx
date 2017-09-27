72b3e77a6bbbd44667ddd958c3d62ba82b93f3d4
khabane <seretsi@users.noreply.github.com>
2017-09-26 20:39:29 -0400

    Loaded lvl 1 background

--- source/LevelOneState.hx
+++ source/LevelOneState.hx
@@ -3,19 +3,38 @@ package;
 import flixel.FlxState;
 import flixel.util.FlxColor;
 import flixel.FlxG;
+import flixel.tile.FlxTilemap;
+import flixel.addons.editors.ogmo.FlxOgmoLoader;
+import flixel.FlxSprite;
 
 class LevelOneState extends FlxState {
 	var ui:UIFunctions;
 	var player:Player = new Player();
+	var _map:FlxOgmoLoader;
+	var _mWalls:FlxTilemap;
 	var cam:Camera = new Camera();
+	var _bg = new FlxSprite( 1, 1); 
 
 	override public function create():Void {
-		add(player);
 		cam.setTarget(player);
 		FlxG.camera = cam;
 		super.create();
 		bgColor = FlxColor.WHITE;
 		ui = new UIFunctions(10);
+		
+		_bg.loadGraphic("assets/art-refined/lv1.png", true, 3200, 2400);
+		_bg.setGraphicSize(800);
+		_bg.screenCenter();
+		add(_bg);
+
+		_map = new FlxOgmoLoader(AssetPaths.levelOneCollisions_oel);
+		_mWalls = _map.loadTilemap(AssetPaths.tiles__png, 16, 16, "walls");
+		_mWalls.follow();
+		/*	_mWalls.setTileProperties(1, FlxObject.NONE);
+		_mWalls.setTileProperties(2, FlxObject.ANY);
+		add(_mWalls);
+		*/
+		add(player);
 		add(ui.getMonologueItem());
 		add(ui.getInteractItem());
 		add(ui.getCouponsItem());
